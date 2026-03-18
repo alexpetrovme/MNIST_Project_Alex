07f72787e10e925bec35ef9ba8f2b28c2d3ea531
alex petrov <alex.me000509@gmail.com>
2026-03-18 11:11:05 -0700

    import transforms in torchvision

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 #Device (CPU or GPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
